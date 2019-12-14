1cd14cfb14f6d3153d9ff23d03e1c6f516f52b9e
Peng Wang <wangpeng@thinkingdata.cn>
2019-12-14 14:59:11 +0800

    SDK: support multi instance for tracking H5 data.
    The instance which has the same app id with H5 data will be used to
    track the data. If there is no matched instance, the default instance
    will be used for tracking.
    
    Signed-off-by: Peng Wang <wangpeng@thinkingdata.cn>

--- thinkingsdk/src/main/java/cn/thinkingdata/android/TDWebAppInterface.java
+++ thinkingsdk/src/main/java/cn/thinkingdata/android/TDWebAppInterface.java
@@ -3,24 +3,64 @@ package cn.thinkingdata.android;
 import android.text.TextUtils;
 import android.webkit.JavascriptInterface;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import cn.thinkingdata.android.utils.TDLog;
 
 public class TDWebAppInterface {
     private final static String TAG = "ThinkingAnalytics.TDWebAppInterface";
 
-    private final ThinkingAnalyticsSDK instance;
+    // if no exist instance has the same token with H5 data, the data will be tracked to default instance.
+    private final ThinkingAnalyticsSDK defaultInstance;
 
-    /** Instantiate the interface and set the context */
+    // for internal use to identify whether the data has been tracked.
+    private class TrackFlag {
+        private boolean tracked;
+
+        void tracked() {
+            tracked = true;
+        }
+
+        boolean shouldTrack() {
+            return !tracked;
+        }
+    }
+
     TDWebAppInterface(ThinkingAnalyticsSDK instance) {
-        this.instance = instance;
+        defaultInstance = instance;
     }
 
     @JavascriptInterface
-    public void thinkingdata_track(String event) {
+    public void thinkingdata_track(final String event) {
         if (TextUtils.isEmpty(event)) {
             return;
         }
+
         TDLog.d(TAG, event);
-        instance.trackFromH5(event);
+
+        try {
+            JSONObject eventData = new JSONObject(event);
+            final String token = eventData.getString("#app_id");
+            final TrackFlag flag = new TrackFlag();
+
+            ThinkingAnalyticsSDK.allInstances(new ThinkingAnalyticsSDK.InstanceProcessor() {
+                @Override
+                public void process(ThinkingAnalyticsSDK instance) {
+                    if (instance.getToken().equals(token)) {
+                        flag.tracked();
+                        instance.trackFromH5(event);
+                    }
+                }
+            });
+
+            // if the H5 data could is not match with any instance, track trough default instance
+            if (flag.shouldTrack()) {
+                defaultInstance.trackFromH5(event);
+            }
+        } catch (JSONException e) {
+            TDLog.w(TAG, "Unexpected exception occurred: " + e.toString());
+        }
+
     }
 }
