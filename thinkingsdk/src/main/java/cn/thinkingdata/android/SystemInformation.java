15fe614cdabdbfcd662aba830c7650bbd3ebd29a
Hale Wang <1334849513@qq.com>
2021-08-19 15:18:54 +0800

    优化屏幕尺寸的采集逻辑

--- thinkingsdk/src/main/java/cn/thinkingdata/android/SystemInformation.java
+++ thinkingsdk/src/main/java/cn/thinkingdata/android/SystemInformation.java
@@ -2,9 +2,11 @@ package cn.thinkingdata.android;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.graphics.Point;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Build;
@@ -13,6 +15,9 @@ import android.provider.Settings;
 import android.telephony.TelephonyManager;
 import android.text.TextUtils;
 import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.Surface;
+import android.view.WindowManager;
 
 import org.json.JSONObject;
 
@@ -99,9 +104,9 @@ class SystemInformation {
             deviceInfo.put(TDConstants.KEY_OS_VERSION, TDUtils.osVersion(mContext));
             deviceInfo.put(TDConstants.KEY_MANUFACTURER, Build.MANUFACTURER);
             deviceInfo.put(TDConstants.KEY_DEVICE_MODEL, Build.MODEL);
-            DisplayMetrics displayMetrics = mContext.getResources().getDisplayMetrics();
-            deviceInfo.put(TDConstants.KEY_SCREEN_HEIGHT, displayMetrics.heightPixels);
-            deviceInfo.put(TDConstants.KEY_SCREEN_WIDTH, displayMetrics.widthPixels);
+            int[] size = getDeviceSize(mContext);
+            deviceInfo.put(TDConstants.KEY_SCREEN_WIDTH, size[0]);
+            deviceInfo.put(TDConstants.KEY_SCREEN_HEIGHT,size[1]);
             String operatorString = getCarrier(mContext);
             deviceInfo.put(TDConstants.KEY_CARRIER, operatorString);
             String androidID = getAndroidID(mContext);
@@ -326,6 +331,66 @@ class SystemInformation {
         }
         return presetProperties;
     }
+    public static int[] getDeviceSize(Context context) {
+        int[] size = new int[2];
+        try {
+            int screenWidth, screenHeight;
+            WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+            Display display = windowManager.getDefaultDisplay();
+            int rotation = display.getRotation();
+            Point point = new Point();
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+                display.getRealSize(point);
+                screenWidth = point.x;
+                screenHeight = point.y;
+            } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+                display.getSize(point);
+                screenWidth = point.x;
+                screenHeight = point.y;
+            } else {
+                screenWidth = display.getWidth();
+                screenHeight = display.getHeight();
+            }
+            size[0] = getNaturalWidth(rotation, screenWidth, screenHeight);
+            size[1] = getNaturalHeight(rotation, screenWidth, screenHeight);
+        } catch (Exception e) {
+            //context.getResources().getDisplayMetrics()这种方式获取屏幕高度不包括底部虚拟导航栏
+            if (context.getResources() != null) {
+                final DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
+                size[0] = displayMetrics.widthPixels;
+                size[1] = displayMetrics.heightPixels;
+            }
+        }
+        return size;
+    }
+
+
+    /**
+     * 根据设备 rotation，判断屏幕方向，获取自然方向宽
+     *
+     * @param rotation 设备方向
+     * @param width 逻辑宽
+     * @param height 逻辑高
+     * @return 自然尺寸
+     */
+    private static int getNaturalWidth(int rotation, int width, int height) {
+        return rotation == Surface.ROTATION_0 || rotation == Surface.ROTATION_180 ?
+                width : height;
+    }
+
+    /**
+     * 根据设备 rotation，判断屏幕方向，获取自然方向高
+     *
+     * @param rotation 设备方向
+     * @param width 逻辑宽
+     * @param height 逻辑高
+     * @return 自然尺寸
+     */
+    private static int getNaturalHeight(int rotation, int width, int height) {
+        return rotation == Surface.ROTATION_0 || rotation == Surface.ROTATION_180 ?
+                height : width;
+    }
+
     private final static  String TAG = "ThinkingAnalytics.SystemInformation";
     private String mAppVersionName;
     private final Map<String, Object> mDeviceInfo;
