7207cbeaf0156cdb31547ff660e15678188b4a88
bugliee <lijin@thinkingdata.cn>
2022-02-07 19:35:53 +0800

    build:修复fps除0异常导致的崩溃

--- thinkingsdk/src/main/java/cn/thinkingdata/android/utils/TDUtils.java
+++ thinkingsdk/src/main/java/cn/thinkingdata/android/utils/TDUtils.java
@@ -725,11 +725,15 @@ public class TDUtils {
                 @Override
                 public void doFrame(long frameTimeNanos) {
                     secondVsync = frameTimeNanos;
-                    long hz = 1000000000 / (secondVsync - firstVsync);
-                    if (hz > 70) {
+                    if (secondVsync <= firstVsync) {
                         fps = 60;
-                    } else {
-                        fps = (int)hz;
+                    }else {
+                        long hz = 1000000000 / (secondVsync - firstVsync);
+                        if (hz > 70) {
+                            fps = 60;
+                        } else {
+                            fps = (int)hz;
+                        }
                     }
                 }
             };
